91e8c83c50f383f2ed2f4198318b5ef58c3b2873
Konrad Bogacz <konrad.bogacz@q-perior.pl>
2022-10-12 14:55:32 +0200

    Create WPF Pokedex project with MVVM pattern Create user control for types list Implement API Fetch repository for types

---
+++ WPF-Pokedex-data-access/Repository/ITypeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPF_Pokedex_data_access.Models;
+
+namespace WPF_Pokedex_data_access.Repository
+{
+    public interface ITypeRepository
+    {
+        public Task<List<TypeListEntity>> getTypes();
+    }
+}
---
+++ WPF-Pokedex-data-access/Repository/TypeRepository.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using WPF_Pokedex_data_access.Models;
+
+namespace WPF_Pokedex_data_access.Repository
+{
+    public class TypeRepository : ITypeRepository
+    {
+        string baseUri = "https://pokeapi.co/api/v2/type";
+        HttpClient client = new HttpClient();
+
+        public async Task<List<TypeListEntity>> getTypes()
+        {
+            HttpResponseMessage response = await client.GetAsync(baseUri);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            TypeListResponse responseResult = JsonConvert.DeserializeObject<TypeListResponse>(responseBody);
+            foreach(var obj in responseResult.results)
+            {
+                obj.name = obj.name.ToUpper();
+            }
+            return responseResult.results;
+        }
+    }
+}
